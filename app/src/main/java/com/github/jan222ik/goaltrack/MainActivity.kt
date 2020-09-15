e3422abca6563f10af88e5218fdc1765989d31ec
Janik Mayr <jan.222.ik@gmail.com>
2020-09-15 21:51:44 +0200

    Initial Compose Application

---
+++ app/src/main/java/com/github/jan222ik/goaltrack/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.github.jan222ik.goaltrack
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import com.github.jan222ik.goaltrack.ui.GoalTrackTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GoalTrackTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    GoalTrackTheme {
+        Greeting("Android")
+    }
+}
